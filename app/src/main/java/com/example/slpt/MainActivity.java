a8c86e5fe3a35ecf644cbed7ecc7cb485ce2386b
KrishanthaBSomarathna <142531941+KrishanthaBSomarathna@users.noreply.github.com>
2023-08-18 13:25:16 +0530

    new

--- app/src/main/java/com/example/slpt/MainActivity.java
+++ app/src/main/java/com/example/slpt/MainActivity.java
@@ -2,13 +2,40 @@ package com.example.slpt;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button manthi,hasheef,ashfak;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        manthi = findViewById(R.id.manthi);
+        hasheef = findViewById(R.id.hasheef);
+        ashfak = findViewById(R.id.ashfak);
+
+        manthi.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, TicketBook.class));
+            }
+        });
+
+        hasheef.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, CargoDriver.class));
+            }
+        });
+        ashfak.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, TaxiBook.class));
+            }
+        });
     }
 }
