0adf5ad9dcc46d35243f338699e4803437357fc7
krishantha <info.krishantha.b@gmail.com>
2023-11-03 03:56:48 +0530

    Report Generate

--- app/src/main/java/com/example/slpt/SA22403810/PassengerMainView.java
+++ app/src/main/java/com/example/slpt/SA22403810/PassengerMainView.java
@@ -1,8 +1,6 @@
 package com.example.slpt.SA22403810;
 
-import android.content.Context;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.LinearLayout;
@@ -13,7 +11,6 @@ import androidx.cardview.widget.CardView;
 
 import com.blogspot.atifsoftwares.animatoolib.Animatoo;
 import com.example.slpt.R;
-import com.example.slpt.SA22403292.PassengerTicketBook;
 import com.example.slpt.SA22403292.TicketBookInitializer;
 import com.example.slpt.SA22404350.PassengerProfile;
 import com.example.slpt.SA22410122.Entry2;
@@ -26,15 +23,14 @@ import java.util.Locale;
 public class PassengerMainView extends AppCompatActivity {
     private TextView clockTextView;
     private TextView dateTextView;
-    LinearLayout profile,home,taxi;
-    private static final String PREFS_NAME = "MyPrefsFile";
-    private static final String FIRST_TIME_KEY = "isFirstTime";
+    LinearLayout profile,home,taxi,report;
+
 
     private CardView routeFinder,liveRadar,ticketbook,taxibook,aboutus;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_passengermainview);
+        setContentView(R.layout.activity_passenger_main_view);
         clockTextView = findViewById(R.id.clockTextView);
         dateTextView = findViewById(R.id.dateTextView);
         routeFinder = findViewById(R.id.busshedules);
@@ -43,26 +39,21 @@ public class PassengerMainView extends AppCompatActivity {
         taxibook = findViewById(R.id.taxibook);
         profile = findViewById(R.id.profile);
         aboutus = findViewById(R.id.aboutus);
+        report = findViewById(R.id.report);
         taxi = findViewById(R.id.taxi);
         home = findViewById(R.id.home);
         updateClockAndDate();
 
+        report.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(),ReportGenerate.class));
+                Animatoo.INSTANCE.animateSlideUp(PassengerMainView.this);
+            }
+        });
 
 
-        // Check if the app is being launched for the first time
-        SharedPreferences prefs = getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE);
-        boolean isFirstTime = prefs.getBoolean(FIRST_TIME_KEY, true);
 
-        if (isFirstTime) {
-            // Set the flag indicating that the app has been launched at least once
-            SharedPreferences.Editor editor = prefs.edit();
-            editor.putBoolean(FIRST_TIME_KEY, false);
-            editor.apply();
-
-            // Open the guide or welcome activity
-            startActivity(new Intent(PassengerMainView.this, PassengerGuide.class));
-            Animatoo.INSTANCE.animateShrink(PassengerMainView.this);
-        }
 
         profile.setOnClickListener(new View.OnClickListener() {
             @Override
